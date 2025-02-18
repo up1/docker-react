a56661615d3e0ba169f0ce29d865e0006a15969d
up1 <somkiat.p@gmail.com>
2025-02-18 14:22:23 +0700

    Docker with playwright

--- ui-test/tests/ui.spec.ts
+++ ui-test/tests/ui.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('เปิดหน้าแรก', async ({ page }) => {
-  await page.goto('http://localhost:8001/');
+  await page.goto('http://ui:');
   await expect(page.locator('#root > h1')).toHaveText('Vite + React');
   await expect(page.locator('#root > div.card > button')).toHaveText('count is 0');
 });
